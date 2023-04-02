85ebbd7970d3de40c2722d28d92de31da0605fe9
Sam Trenter <sntrenter@gmail.com>
2023-04-02 15:29:44 -0500

    can hit all movie pages

--- selWebDriver.py
+++ selWebDriver.py
@@ -5,6 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from cookies import cookies
 
+#get all links in an h5 from the page using beautiful soup
+def getLinks(url):
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('h5')
+    return links
+
 # Configure the web driver (in this case, Chrome)
 options = ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
@@ -21,12 +28,40 @@ continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains
 if continue_button:
     continue_button.click()
 
-# Extract the page source and parse it with BeautifulSoup
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html.parser')
-links = soup.find_all('a', href=True)
-print(links)
 
+#if a button with the text "Movies" exists, click it
+continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Movies')]")))
+if continue_button:
+    continue_button.click()
+
+#if a button with the text "IMDB Rating" exists, click it
+continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'IMDB Rating')]")))
+if continue_button:
+    continue_button.click()
+
+disabled = False
+while not disabled: 
+    #get current html
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('h5')
+    print(links)
+    pagination = soup.find('ul', class_='pagination')
+
+    # Find the last li element in the pagination list
+    last_li = pagination.find_all('li')[-1]
+
+    # Check if the last li element is disabled
+    if 'disabled' in last_li.get('class', []):
+        print('The last element is disabled')
+        disabled = True
+        break;
+    else:
+        print('The last element is enabled')
+        nextPageBtn = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), '»')]")))
+        nextPageBtn.click()
+    break
+print("End")
 # Close the web driver
 driver.quit()
 
